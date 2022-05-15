ada52aa0784372afbd89803c1860351de751aa13
JokeMort <turbiaktomasz@gmail.com>
2022-05-15 19:26:00 +0200

    .

---
+++ cmake-build-debug/CMakeFiles/hashowanie.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hashowanie.dir/hash.cpp.obj"
+  "CMakeFiles/hashowanie.dir/main.cpp.obj"
+  "hashowanie.exe"
+  "hashowanie.exe.manifest"
+  "hashowanie.pdb"
+  "libhashowanie.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hashowanie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
